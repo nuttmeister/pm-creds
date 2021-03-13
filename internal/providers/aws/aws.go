39f62f88a6e70a2b9b6584984bbaebfef9ec1329
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-13 15:58:49 +0100

    Added comments to aws provider package.

--- internal/providers/aws/aws.go
+++ internal/providers/aws/aws.go
@@ -49,6 +49,8 @@ func read(config map[string]interface{}, option string) ([]string, error) {
 	return slice, nil
 }
 
+// Provider fullfills the types.Provider interface and can be used as
+// an provider by the providers package.
 type Provider struct {
 	name string
 
@@ -56,14 +58,20 @@ type Provider struct {
 	configs []string
 }
 
+// Name returns the provider name.
+// Returns string.
 func (p *Provider) Name() string {
 	return p.name
 }
 
+// Type returns the provider type.
+// Returns string.
 func (p *Provider) Type() string {
 	return providerType
 }
 
+// Get will retrieve profile name from provider p.
+// Returns *types.Profile and error.
 func (p *Provider) Get(name string) (types.Profile, error) {
 	shared, err := config.LoadSharedConfigProfile(
 		context.Background(),
@@ -95,15 +103,21 @@ func (p *Provider) Get(name string) (types.Profile, error) {
 	return &Profile{name: name, payload: payload}, nil
 }
 
+// Profile fullfills the types.Profile interface and can be used
+// as an profile by the providers package.
 type Profile struct {
 	name    string
 	payload []byte
 }
 
+// Name returns the profile name.
+// Returns string.
 func (p *Profile) Name() string {
 	return p.name
 }
 
+// Payload returns the profile payload as JSON.
+// Returns []byte.
 func (p *Profile) Payload() []byte {
 	return p.payload
 }
