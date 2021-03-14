ef4a871986caf96de154d9935e38de606b33fb94
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-14 10:34:44 +0100

    Changed to using message struct to have done channel on logging package.

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -30,15 +30,24 @@ func Lb() string {
 }
 
 type Logger struct {
-	messages chan string
+	messages chan *message
+	time     timeInterface
 	stdOut   io.Writer
 	stdErr   io.Writer
+}
+
+// message is used to print text and sends done
+// once the text has been printed to l.stdOut.
+type message struct {
+	text string
+	done chan bool
 }
 
 // New creates a new Logger.
 func New() *Logger {
 	logger := &Logger{
-		messages: make(chan string),
+		messages: make(chan *message),
+		time:     &realTime{},
 		stdOut:   colorable.NewColorable(os.Stdout),
 		stdErr:   colorable.NewColorable(os.Stderr),
 	}
@@ -49,38 +58,73 @@ func New() *Logger {
 
 // Print will print message with format and prefix of current date and time.
 func (l *Logger) Print(format string, a ...interface{}) {
-	l.messages <- fmt.Sprintf("%s: %s", now(), fmt.Sprintf(format, a...))
+	msg := &message{
+		text: fmt.Sprintf("%s: %s", l.now(), fmt.Sprintf(format, a...)),
+		done: make(chan bool),
+	}
+	l.messages <- msg
+	<-msg.done
 }
 
 // Notice will print message with format and prefix of current date and time in green color.
 func (l *Logger) Notice(format string, a ...interface{}) {
-	l.messages <- green.Sprintf("%s: %s", now(), fmt.Sprintf(format, a...))
+	msg := &message{
+		text: green.Sprintf("%s: %s", l.now(), fmt.Sprintf(format, a...)),
+		done: make(chan bool),
+	}
+	l.messages <- msg
+	<-msg.done
 }
 
 // Warning will print message with format and prefix of current date and time in yellow color.
 func (l *Logger) Warning(format string, a ...interface{}) {
-	l.messages <- yellow.Sprintf("%s: %s", now(), fmt.Sprintf(format, a...))
+	msg := &message{
+		text: yellow.Sprintf("%s: %s", l.now(), fmt.Sprintf(format, a...)),
+		done: make(chan bool),
+	}
+	l.messages <- msg
+	<-msg.done
 }
 
 // Alert will print message with format and prefix of current date and time in red color.
 func (l *Logger) Alert(format string, a ...interface{}) {
-	l.messages <- red.Sprintf("%s: %s", now(), fmt.Sprintf(format, a...))
+	msg := &message{
+		text: red.Sprintf("%s: %s", l.now(), fmt.Sprintf(format, a...)),
+		done: make(chan bool),
+	}
+	l.messages <- msg
+	<-msg.done
 }
 
 // Error will print message with format and prefix of current date and time and then exit 1.
 func (l *Logger) Error(err error) {
-	fmt.Fprintf(l.stdErr, "%s: %s", now(), fmt.Sprintf("%s%s", err.Error(), Lb()))
+	fmt.Fprintf(l.stdErr, "%s: %s", l.now(), fmt.Sprintf("%s%s", err.Error(), Lb()))
 	os.Exit(1)
 }
 
 // print will read from l.messages and print to l.stdOut.
 func (l *Logger) print() {
 	for {
-		fmt.Fprint(l.stdOut, <-l.messages)
+		msg := <-l.messages
+		fmt.Fprint(l.stdOut, msg.text)
+		msg.done <- true
 	}
 }
 
+// timeInterface implements Now.
+type timeInterface interface {
+	Now() time.Time
+}
+
+// realTime satisfies the timeInterface.
+type realTime struct{}
+
+// Now returns time.Time from time.Now().
+func (rt *realTime) Now() time.Time {
+	return time.Now()
+}
+
 // now returns current date and time in dateFormat.
-func now() string {
-	return time.Now().Format(dateFormat)
+func (l *Logger) now() string {
+	return l.time.Now().Format(dateFormat)
 }
