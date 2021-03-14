7e9030f48fb82db653e4286405cae274772987f0
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-14 20:09:04 +0100

    Create defaults and certificates.

--- pm-creds/main.go
+++ pm-creds/main.go
@@ -2,66 +2,37 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/nuttmeister/pm-creds/internal/certs"
-	"github.com/nuttmeister/pm-creds/internal/file"
 	"github.com/nuttmeister/pm-creds/internal/logging"
 	"github.com/nuttmeister/pm-creds/internal/providers"
 	"github.com/nuttmeister/pm-creds/internal/server"
 )
 
-var logger = logging.New()
+var (
+	logger  = logging.New()
+	home, _ = os.UserHomeDir()
+	cfgDir  = filepath.Join(home, ".pm-creds")
+)
 
 func main() {
-	dir, _ := os.UserHomeDir()
-	providersFile := filepath.Join(dir, "/.pm-creds/providers.toml")
-	configFile := filepath.Join(dir, "/.pm-creds/config.toml")
-	createCerts, overwrite := false, false
-
-	flag.StringVar(&providersFile, "providers", providersFile, "Location of the providers file")
-	flag.StringVar(&configFile, "config", configFile, "Location of the config file")
-	flag.BoolVar(&createCerts, "certs", createCerts, "If certificates should be generated")
-	flag.BoolVar(&overwrite, "overwrite", overwrite, "If new certificates should overwrite old (if they exist)")
+	createConfig, createCerts, overwrite := false, false, false
+	flag.StringVar(&cfgDir, "config-dir", cfgDir, "Location of the config files")
+	flag.BoolVar(&createConfig, "create-config", createConfig, "If the default config should be created")
+	flag.BoolVar(&createCerts, "create-certs", createCerts, "If certificates should be generated")
+	flag.BoolVar(&overwrite, "overwrite", overwrite, "If new config/certificates should overwrite old")
 	flag.Parse()
 
 	createCertificates(createCerts, overwrite)
+	createConfiguration(createConfig, overwrite)
 
-	providers, err := providers.Load(providersFile)
+	providers, err := providers.Load(cfgDir)
 	if err != nil {
 		logger.Error(err)
 	}
 
-	if err := server.Start(configFile, providers, logger); err != nil {
+	if err := server.Start(cfgDir, providers, logger); err != nil {
 		logger.Error(err)
-	}
-}
-
-// createCertificates will create certificates if createCerts is true.
-// If certificate files already exists and overwrite is true existing
-// files will be overwritten.
-func createCertificates(createCerts bool, overwrite bool) {
-	if createCerts {
-		certFiles := []string{"./ca-key.pem", "./ca-cert.pem", "./server-key.pem", "./server-cert.pem"}
-		exists, err := file.CheckFilesExists(certFiles)
-		if err != nil {
-			logger.Error(err)
-		}
-
-		if exists && !overwrite {
-			logger.Error(fmt.Errorf("certificate files already exist! use --overwrite or delete them first"))
-		}
-
-		if err := certs.Create(certFiles[0], certFiles[1], certFiles[2], certFiles[3]); err != nil {
-			logger.Error(err)
-		}
-
-		logger.Print(
-			"generated %q, %q, %q, %q. please setup pm-creds and postman with these%s",
-			certFiles[0], certFiles[1], certFiles[2], certFiles[3], logging.Lb(),
-		)
-		os.Exit(0)
 	}
 }
