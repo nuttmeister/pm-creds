7406148fb2785125f9575e39fa1ff875e4b24671
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-15 19:38:20 +0100

    Added verbose of sign.

--- build.sh
+++ build.sh
@@ -12,11 +12,12 @@ FILENAME="pm-creds-${TAG}-${FRIENDLY}-${GOOS}"
 
 GOOS=${GOOS} GOARCH=${GOARCH} go build -o ../dist/${FILENAME}/pm-creds
 if [ "${GOOS}" = "darwin" ]; then
-	codesign --force -s "${AC_APPID}" --timestamp ../dist/${FILENAME}/pm-creds -v
+	codesign --force -s "${AC_APPID}" --timestamp ../dist/${FILENAME}/pm-creds
+	codesign -v ../dist/${FILENAME}/pm-creds
 fi
 
 zip -j ../dist/${FILENAME}.zip ../dist/${FILENAME}/pm-creds
 
 if [ "${GOOS}" = "darwin" ]; then
-	xcrun altool --notarize-app --primary-bundle-id "se.execit.pm-creds" -u "${AC_USERNAME}" -p "${AC_PASSWORD}" -t osx -f ../dist/${FILENAME}.zip
+	xcrun altool --notarize-app --primary-bundle-id "se.execit.pm-creds.zip" -u "${AC_USERNAME}" -p "${AC_PASSWORD}" -t osx -f ../dist/${FILENAME}.zip
 fi
