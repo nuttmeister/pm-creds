cec965e63fbc9be953d4e92be0ce33f58211e20c
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-15 20:00:19 +0100

    Use ditto instead of zip. Change notarization.

--- build.sh
+++ build.sh
@@ -10,14 +10,23 @@ fi
 
 FILENAME="pm-creds-${TAG}-${FRIENDLY}-${GOOS}"
 
+# Build
+mkdir -p dist/${FILENAME}
+cd pm-creds
 GOOS=${GOOS} GOARCH=${GOARCH} go build -o ../dist/${FILENAME}/pm-creds
-if [ "${GOOS}" = "darwin" ]; then
-	codesign --force -s "${AC_APPID}" --timestamp ../dist/${FILENAME}/pm-creds
-	codesign --verify --verbose ../dist/${FILENAME}/pm-creds
-fi
 
-zip -j ../dist/${FILENAME}.zip ../dist/${FILENAME}/pm-creds
+# Notarize and/or zip.
+cd ../dist/${FILENAME}
 
 if [ "${GOOS}" = "darwin" ]; then
-	xcrun altool --notarize-app --primary-bundle-id "se.execit.pm-creds.zip" -u "${AC_USERNAME}" -p "${AC_PASSWORD}" -t osx -f ../dist/${FILENAME}.zip
+	codesign --deep --force -s "${AC_APPID}" --timestamp pm-creds
+	codesign --verify --verbose ${FILENAME}/pm-creds
+	ditto -c -k --keepParent --rsrc pm-creds ${FILENAME}.zip
+	xcrun altool --notarize-app --primary-bundle-id "se.execit.pm-creds.zip" -u "${AC_USERNAME}" -p "${AC_PASSWORD}" -t osx -f ${FILENAME}.zip
+else
+	ditto -c -k --keepParent --rsrc pm-creds ${FILENAME}.zip
 fi
+
+rm -rf pm-creds
+
+cd ../../
