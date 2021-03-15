305b58da6ee63389e8216b96be1cf2058488e7af
Anders Wallin <anders.wallin@danielwellington.com>
2021-03-15 19:16:54 +0100

    test build script.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+GOOS=${1}
+GOARCH=${2}
+
+FRIENDLY=${GOOS}
+if [ ${FRIENDLY} = "darwin" ]; then
+	FRIENDLY=macOS
+fi
+
+FILENAME="pm-creds-${TAG}-${FRIENDLY}-${GOOS}"
+
+GOOS=${GOOS} GOARCH=${GOARCH} go build -o ../dist/${FILENAME}/pm-creds
+if [ "${GOOS}" = "darwin" ]; then
+	codesign --force -s "${AC_APPID}" --timestamp ../dist/${FILENAME}/pm-creds -v
+fi
+
+zip -j ../dist/${FILENAME}.zip ../dist/${FILENAME}/pm-creds
+
+if [ "${GOOS}" = "darwin" ]; then
+	xcrun altool --notarize-app --primary-bundle-id "se.execit.pm-creds" -u "${AC_USERNAME}" -p "${AC_PASSWORD}" -t osx -f ../dist/${FILENAME}.zip
+fi
